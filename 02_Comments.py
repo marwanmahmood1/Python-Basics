fdb2513ce2b3db342cf1e2b1c573c152fa5f2385
Marwan Mahmoud <maro.mahmoud2020@gmail.com>
2025-07-19 10:57:15 +0300

    Add files via upload

---
+++ 02_Comments.py
@@ -0,0 +1,22 @@
+# ========================
+#        COMMENTS
+# ========================
+
+# This is a comment. Python ignores it.
+# Comments are useful for explaining your code.
+
+# You can write a comment like this:
+# This line won't do anything when you run the code.
+
+print("Comments don't show up when the code runs")  # This is an inline comment
+
+# Python doesn't have multi-line comments like other languages,
+# but you can use a triple-quoted string as a workaround:
+
+"""
+This is a multi-line comment
+It's actually a multi-line string,
+but Python will ignore it if it's not assigned to anything.
+"""
+
+print("Done with comments!")
