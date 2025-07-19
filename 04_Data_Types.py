3488f9876cef127e80f06a7085408147a3ae6297
Marwan Mahmoud <maro.mahmoud2020@gmail.com>
2025-07-19 11:05:21 +0300

    Add files via upload

---
+++ 04_Data_Types.py
@@ -0,0 +1,23 @@
+# =========================
+#       DATA TYPES
+# =========================
+
+# Python has several built-in data types. Here are the most common:
+
+# Text, also named string (str)
+greeting = "Hello"
+
+# Numbers
+age = 25            # This is an integer (int)
+height = 1.70       # This is a float type of numerical data (float)
+
+# Boolean (True or False)
+is_happy = True
+
+# None (this represents no value)
+nothing = None
+
+# You can check the type of any variable using 'type()'
+print(type(greeting))  # <class 'str'>
+print(type(age))       # <class 'int'>
+print(type(is_happy))  # <class 'bool'>
