2e73be2a0d0ad3035696fe97c743c27ac3b0ae72
Marwan Mahmoud <maro.mahmoud2020@gmail.com>
2025-09-06 01:51:43 +0300

    Reorganized repo into structured folders and renamed files

--- 01-Introduction/03-Variables and Data Types.py
+++ 01-Introduction/03-Variables and Data Types.py
@@ -1,4 +1,22 @@
 # =========================
+#        VARIABLES
+# =========================
+
+# Variables are labels we assign to values using the '=' sign.
+
+# Example:
+name = "Adam"
+age = 30
+height = 1.75
+is_student = True
+
+# You can print them:
+print(name)
+print(age)
+print(height)
+print(is_student)
+
+# =========================
 #       DATA TYPES
 # =========================
 
